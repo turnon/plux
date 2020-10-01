a55c8f72841b8e95aa334a7df1c0b00156412196
ken <block24block@gmail.com>
2020-10-01 14:59:34 +0000

    read server name in prepare/process method

--- lib/plux/engine.rb
+++ lib/plux/engine.rb
@@ -1,7 +1,10 @@
 module Plux
   class Engine
 
+    attr_reader :name
+
     def initialize(name, thread: 1)
+      @name = name
       @server = Server.new(name, thread: thread).boot(self)
     end
 
--- lib/plux/version.rb
+++ lib/plux/version.rb
@@ -1,3 +1,3 @@
 module Plux
-  VERSION = "0.1.7"
+  VERSION = "0.1.8"
 end
